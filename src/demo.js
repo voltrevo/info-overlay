9441c3142057eb973c765622d807b396c196a0aa
Andrew Morris <voltrevo@gmail.com>
2015-06-07 22:49:55 +1000

    Add nice background and lorem ipsum.

--- src/demo.js
+++ src/demo.js
@@ -1,8 +1,24 @@
 'use strict'
 
-var iconOverlay = require('./index')
+var loremIpsum = require('./lorem-ipsum.html')
+var IconOverlay = require('./index')
 
-window.addEventListener('load', function() {
-  document.body.style.backgroundColor = 'blue'
-  document.body.appendChild(iconOverlay().icon)
+var windowLoaded = new Promise(function(resolve) {
+  window.addEventListener('load', resolve)
+})
+
+windowLoaded.then(function() {
+  document.body.style.background = (
+    'url(https://lh3.googleusercontent.com/-1xZqgaRDIec/Tg1dMJq1vBI/AAAAAAAAALc/7m0Tpv2htVc/s1920-w1920-h1080-c/071227-4144-PtLomaReef.jpg)'
+  )
+
+  document.body.style.backgroundSize = 'cover'
+
+  var iconOverlay = IconOverlay()
+
+  document.body.appendChild(iconOverlay.icon)
+
+  iconOverlay.overlay.innerHTML = loremIpsum().innerHTML
+
+  document.body.style.height = '300vh'
 })
