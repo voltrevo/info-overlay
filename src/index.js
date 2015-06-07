5d20ac212fe1fa8a835d8ea6b409a5969eeba7d4
Andrew Morris <voltrevo@gmail.com>
2015-06-07 18:03:20 +1000

    Use getOutsideClick and promises in demo

--- src/index.js
+++ src/index.js
@@ -1,54 +1,38 @@
 'use strict'
 
+var delay = require('./delay')
+var getOutsideClick = require('./getOutsideClick')
 var infoIcon = require('info-icon')
+var infoOverlay = require('./overlay.html')
 
 require('./overlay.css')
-var infoOverlay = require('./overlay.html')
 
 module.exports = function() {
   var icon = infoIcon()
   var overlay = infoOverlay()
 
   var active = false
-  var clickCounter = 0
 
-  var activate = function() {
-    icon.removeEventListener('click', activate)
-    active = true
-    clickCounter = 1
-
-    document.body.appendChild(overlay)
-    setTimeout(function() {
-      overlay.style.opacity = 0.8
-    }, 50)
-  }
-
-  var deactivate = function() {
-    overlay.style.opacity = 0
-    active = false
-    setTimeout(function() {
-      document.body.removeChild(overlay)
-      icon.addEventListener('click', activate)
-    }, 550)
-  }
-
-  overlay.addEventListener('click', function() {
-    clickCounter++
-  })
-
-  window.addEventListener('click', function() {
-    if (!active) {
+  icon.addEventListener('click', function() {
+    if (active) {
       return
     }
 
-    clickCounter--
+    active = true
 
-    if (clickCounter < 0) {
-      deactivate()
-    }
+    document.body.appendChild(overlay)
+
+    delay(50).then(function() {
+      overlay.style.opacity = 0.8
+      return getOutsideClick(overlay)
+    }).then(function() {
+      overlay.style.opacity = 0
+      return delay(550)
+    }).then(function() {
+      document.body.removeChild(overlay)
+      active = false
+    })
   })
-
-  icon.addEventListener('click', activate)
 
   return {
     icon: icon,
